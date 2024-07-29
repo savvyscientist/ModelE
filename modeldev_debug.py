71a304dc3883f2a64210597ea50eccf8e8e7a219
Keren Mezuman <kerenmezuman@Kerens-MacBook-Pro-2.local>
2024-07-29 13:51:20 -0400

    improved functionality: automated processes (e.g. read in scaling factor), added a calculation for total, added units to outputs, revised calculation for cbarmx to based on percentiles.

--- modeldev_debug.py
+++ modeldev_debug.py
@@ -5,23 +5,23 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import pandas as pd
 import sys
+import re
 
 #devtype = 'setuphour'
-devtype = 'month'
-#devtype = 'pyrEsource'
+#devtype = 'month'
+devtype = 'pyrEsource'
 if devtype == 'month':
-    #pyrEsource 
-    month='JAN1950'
+    month = 'JAN1950'
     outputpath = '/discover/nobackup/kmezuman/plots/fire_repository/Development/setuphour_bug/'
     simlist = ['E6TomaF40pyrEold25', 'E6TomaF40pyrE25']
     index_pairs = [(0, 1)]
 elif devtype == 'pyrEsource':
     outputpath = '/discover/nobackup/kmezuman/plots/fire_repository/Development/pyrEsource/'
-    month='ANN1996'
+    month = 'ANN1996'
     simlist = ['E6TomaF40intpyrEtest2', 'E6TomaF40intpyrEtest']
     index_pairs = [(0, 1)]
 elif devtype == 'setuphour':
-    month='PARTIAL'
+    month = 'PARTIAL'
     outputpath = '/discover/nobackup/kmezuman/plots/fire_repository/Development/setuphour_bug/Partial/'
     simlist = ['E6TomaF40pyrEold241', 'E6TomaF40pyrEold25', 'E6TomaF40pyrE241', 'E6TomaF40pyrE25']
     index_pairs = [(0, 1), (2, 3), (2, 0), (3, 1)]
@@ -29,15 +29,72 @@ else:
     sys.exit(f'incompatibel type: {devtype}')
 
 simpath = '/discover/nobackup/kmezuman/'
-var_list = ['fireCount']#, 'BA_tree', 'BA_grass', 'BA_shrub', 'FLAMM', 'f_ignCG', 'f_ignHUMAN', 'CO_biomass_src']
+
+# Define the baseline variables
+#base_variables = ["FLASH", "CtoG", "FLAMM", "fireCount", "f_ignCG", "f_ignHUMAN", "BA_tree", "BA_shrub", "BA_grass"]
+base_variables = ["fireCount","FLAMM"]
+s_to_yr = 60.*60.*24.*365.
+kg_to_Tg = 1.E-9
+
+# Define the tracer sources and tracers
+tracer_sources = ["AGRI_CMIP6_src", "PEAT_CMIP6_src", "biomass_src", "pyrE_src"]
+#tracers = ["CO", "BCB"]
+tracers = ["CO", "BCB", "OCB", "Alkenes", "Paraffin", "SO2", "NOx", "NH3"]
+include_tracers = True 
+
+# Define the carbon cycle tracer
+ccycle = "CO2n"
+include_ccycle = True
+
+intch4 = 'CH4'
+include_intch4 = False
+
+# Initialize the variable list with baseline variables
+var_list = base_variables.copy()
+
+# Add tracer-related variables
+if include_tracers:
+    for tracer in tracers:
+        for source in tracer_sources:
+            var_list.append(f"{tracer}_{source}")
+
+# Add carbon cycle-related variables
+if include_ccycle:
+    for source in tracer_sources:
+        var_list.append(f"{ccycle}_{source}")
+
+# Add methane related variables
+if include_intch4:
+    for source in tracer_sources:
+        var_list.append(f"{intch4}_{source}")
+
 hemis_glob_ind = 2
 data = {}
 
-# Loop over relevant variables
+def extract_scaling_factor(units):
+    """
+    Extracts the scaling factor from the units string and converts it to a number.
+    For example, '10^-3 kg/m^2/s' will return 0.001 and 'kg/m^2/s'.
+    """
+    match = re.match(r"^(10\^(-?\d+)|[-+]?\d*\.?\d+([eE][-+]?\d+)?)\s*(.*)$", units)
+    if match:
+        if match.group(1).startswith("10^"):
+            scaling_factor = float(10) ** float(match.group(2))
+        else:
+            scaling_factor = float(match.group(1))
+        unit = match.group(4)
+        return scaling_factor, unit
+    return 1.0, units  # Default scaling factor is 1 if not specified
+
+# A function that changes a key's name in a dictionary
+def change_dict_key(d, old_key, new_key, default_value=None):
+    d[new_key] = d.pop(old_key, default_value)
+
+# Loop over simulations and variables to process data
 for s, sim in enumerate(simlist):
     for v, var in enumerate(var_list):
-    # Set ftype depending on the var 
-        if var.endswith('biomass_src') or var.endswith('pyrE_src'):
+        # Determine the file type based on variable name
+        if var.endswith('_src'):
             ftype = 'taij'
         else:
             ftype = 'aij'
@@ -49,55 +106,142 @@ for s, sim in enumerate(simlist):
             if v == 0 and s == 0:
                 lat = dataset.variables['lat'][:]
                 lon = dataset.variables['lon'][:]
-                hemis_nan_array = np.full(3, np.nan)
-                nan_mat = np.full((len(lat), len(lon)), np.nan)
-            # Read hemis values
+                grid_cell_area = dataset.variables['axyp'][:]
+                #hemis_nan_array = np.full(1, np.nan)
+                hemis_nan_array = np.full(1, 0.)
+                #nan_mat = np.full((len(lat), len(lon)), np.nan)
+                nan_mat = np.full((len(lat), len(lon)), 0.)
+
+            # Extract units of the variable and the scaling factor
+            units = dataset.variables[var].units
+            scaling_factor, unit = extract_scaling_factor(units)
+            print(var,scaling_factor,units)
+
+            #Calculate global mean values
             hemis_var = dataset.variables[var + '_hemis']
+            hemis_var = hemis_var[hemis_glob_ind]
+            # Handle missing values for hemis_var 
+            #the line commented below has a bug
+            #missing_value = hemis_var.missing_value #if 'missing_value' in hemis_var.ncattrs() else None 
+            missing_value = -1.E+30
+            if missing_value is not None: 
+                hemis_var = np.where(hemis_var <= missing_value, hemis_nan_array, hemis_var) 
+            hemis_var *= scaling_factor
+
             # Read 2D values
             two_d_var = dataset.variables[var]
-            # Handle missing values for hemis_var 
-            fill_value = hemis_var._FillValue if '_FillValue' in hemis_var.ncattrs() else None 
-            missing_value = hemis_var.missing_value if 'missing_value' in hemis_var.ncattrs() else None 
-            hemis_val = hemis_var[:] 
-            if missing_value is not None: 
-                hemis_val = np.where(hemis_val <= missing_value, hemis_nan_array, hemis_val) 
             # Handle missing values for two_d_var 
-            missing_value_2d = two_d_var.missing_value if 'missing_value' in two_d_var.ncattrs() else None 
+            #the line commented below has a bug
+            #missing_value_2d = two_d_var.missing_value if 'missing_value' in two_d_var.ncattrs() else None 
+            missing_value_2d = missing_value
             two_d_val = two_d_var[:] 
             if missing_value_2d is not None: 
                 two_d_val = np.where(two_d_val <= missing_value_2d, nan_mat, two_d_val) 
-            data[(var, s)] = {"hemis": hemis_val[hemis_glob_ind], "2D": two_d_val}
+            two_d_val *= scaling_factor
+
+            #Calculate global total values
+            if "m^-2" in unit:
+                 total = np.nansum(two_d_val * grid_cell_area)
+                 total_unit = unit.replace("m^-2","")
+            elif "m-2" in unit:
+                 total = np.nansum(two_d_val * grid_cell_area)
+                 total_unit = re.sub(r'\bm\-2\b', '', unit).strip()
+            else:
+                 total = np.nansum(two_d_val)
+                 total_unit = unit
+
+            if "s-1" in unit:
+                 total *= s_to_yr
+                 total_unit = total_unit.replace("s-1","yr-1").strip()
+
+            if "kg" in unit:
+                 total *= kg_to_Tg 
+                 total_unit = total_unit.replace("kg","Tg").strip()
+
+
+            # Store data in dictionary
+            data[(var, s)] = {
+                    "mean": hemis_var,
+                    "total": total, 
+                    "total_units": total_unit,
+                    "2D": two_d_val,
+                    "units": unit
+            }
+
+            #print(hemis_var.shape, two_d_val.shape)
+            dataset.close()
+
         except FileNotFoundError:
-            print(f"File {filename} not found.")
+            print(f"File not found: {filename}")
+
         except Exception as error:
             print(f"An exception occurred while reading from {filename}: {str(var)}. {error}")
-for key, value in data.items():
-    var_name, sim_index = key
-    hemis_shape = value["hemis"].shape
-    two_d_shape = value["2D"].shape
-    #print(f"Variable: {var_name}, Simuation Index: {sim_index}")
-    #print(f"  Hemis Shape: {hemis_shape}")
-    #print(f"  2D Shape: {two_d_shape}")
+            data[(var, s)] = {"hemis": np.zeros(1,dtype=float), "2D": np.zeros((len(lat), len(lon)))}
+            # Store data in dictionary
+            data[(var, s)] = {
+                    "mean": np.zeros(1,dtype=float),
+                    "total": np.zeros(1,dtype=float), 
+                    "total_units": [],
+                    "2D": np.zeros((len(lat), len(lon))),
+                    "units": []
+            }
+
+#for key, value in data.items():
+#    var_name, sim_index = key
+#    two_d_shape = value["2D"].shape
+#    print(f"Variable: {var_name}, Simuation Index: {sim_index}")
+#    print(f"  2D Shape: {two_d_shape}")
 
 # Calculate differences
-hemis_diffs_dict = {var: [] for var in var_list}
+global_diffs_dict = {var: [] for var in var_list}
 simnames = []
 for i, j in index_pairs:
     if i-j > 1:
         simname = f"({simlist[i]} - {simlist[j]})/{simlist[j]}"
     else:
         simname = f"{simlist[i]} - {simlist[j]}"
-    print(simname)
+    #print(simname)
     simnames.append(simname)
     for v, var in enumerate(var_list):
         if i-j > 1:
-            hemis_diff = (data[(var, i)]["hemis"] - data[(var, j)]["hemis"])/data[(var, j)]["hemis"]
+            global_diff = (data[(var, i)]["total"] - data[(var, j)]["total"])/data[(var, j)]["total"]
         else:
-            hemis_diff = data[(var, i)]["hemis"] - data[(var, j)]["hemis"]
-        hemis_diffs_dict[var].append(hemis_diff)
+            global_diff = data[(var, i)]["total"] - data[(var, j)]["total"]
+        global_diffs_dict[var].append(global_diff)
+
+#print(data.items())
+#print(data.keys())
+#print(data.values())
+# Print and save the totals into a text file
+totals_dict = {}
+# Extract the "total" values for each variable and simulation 
+for (var, sim_index), values in data.items():
+    #print(var,sim_index,simlist[sim_index],values["total"])
+    var_with_unit = f"{var} [{values['total_units']}]"
+    #print(var_with_unit)
+    if var not in totals_dict:
+        totals_dict[var] = {}
+    totals_dict[var][simlist[sim_index]] = values["total"]
+#the construction of totals_dict has a bug
+#add a part that checks if 'total_units' are [] and if so rename the dictionary
+#key:
+#    if values['total_units']
+#    change_dict_key(totals_dict, var, var_with_unit, default_value=None):
+
+# Create a DataFrame from the totals_dict 
+df_totals = pd.DataFrame(totals_dict) 
+# Transpose the DF to have simulations as a columns and variables as rows
+df_totals = df_totals.T
+# Print the DataFrame to check the result 
+print(df_totals)
+formatted_table = df_totals.to_string()
+output_file_path = (f"{outputpath}fire_total.txt")
+with open(output_file_path, 'w') as file:
+    file.write(formatted_table)
+print(f"Totals saved in {output_file_path}")
 
 # Print and save the difference into a text file
-df = pd.DataFrame(hemis_diffs_dict, index = simnames)
+df = pd.DataFrame(global_diffs_dict, index = simnames)
 formatted_table = df.to_string()
 output_file_path = (f"{outputpath}fire_total_diff.txt")
 with open(output_file_path, 'w') as file:
@@ -107,7 +251,7 @@ print(f"Differences saved in {output_file_path}")
 #Plot 2D differences
 for var in var_list:
     #plt.subplots_adjust(hspace=0.001)
-    if devtype == 'month':
+    if devtype == 'month' or devtype == 'pyrEsource':
         plots = [
             (0, 0, (var, 0), simlist[0]), # A 
             (0, 1, (var, 1), simlist[1]), # B 
@@ -129,31 +273,32 @@ for var in var_list:
         fig.delaxes(axes[2,2])
     else:
         sys.exit(f'incompatibel type: {devtype}')
-    axes=axes.flatten()[:len(plots)]
+    axes = axes.flatten()[:len(plots)]
     for i, (row, col, plot_info, label) in enumerate(plots): 
         ax = axes[i] 
         ax.set_extent([-180, 180, -90, 90], crs=ccrs.PlateCarree())
+
+        #calculate the 95th percentile of plot_data and use that as the colorbar max
 
         if isinstance(plot_info[0], str): # Plotting actual values A, B, A', B' 
             var, idx = plot_info
             plot_data = data[(var, idx)]["2D"] 
             cmap = 'jet'
-            vmax = 0.7 * np.nanmax(plot_data)
+            vmax = np.percentile(plot_data, 97)  # Use the 95th percentile for colorbar max
             vmin = 0.
         else: # Plotting differences 
             i, j = plot_info          
             plot_data = data[(var, i)]["2D"] - data[(var, j)]["2D"] 
             cmap='bwr'
-            max_diff = 0.7 * np.nanmax(np.abs(plot_data))
+            max_diff = np.percentile(np.abs(plot_data), 97)  # Use the 95th percentile for colorbar max
             vmax = max_diff
             vmin = -max_diff
 
-        total = np.nansum(plot_data)
-        #total_str = f"{total:.2f}"
-        ax.set_title(f"{var}: {label}\nTotal: {total}", y=1.10)
+        ax.set_title(f"{var}: {label}\nTotal: {data[(var, idx)]['total']} {data[(var, idx)]['total_units']}", y=1.10)
         longitudes, latitudes = np.meshgrid(lon, lat)
         plot = ax.pcolormesh(longitudes, latitudes, plot_data, transform=ccrs.PlateCarree(), cmap=cmap, vmin=vmin, vmax=vmax)
         cbar = fig.colorbar(plot, ax=ax, orientation='horizontal',fraction=0.046, pad=0.08)
+        #fix units
         cbar.set_label('units',labelpad=5,fontsize=10)
         ax.coastlines()
         ax.add_feature(cfeature.BORDERS)
@@ -168,3 +313,4 @@ for var in var_list:
         #fig.colorbar(plot, ax=ax, orientation='horizontal', fraction=0.046, pad=0.04)
 
     fig.savefig(f"{outputpath}{var}_eval.eps")
+    plt.close(fig)
