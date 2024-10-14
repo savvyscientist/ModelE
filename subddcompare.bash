ae4b1a9764fa64baea98d052bfcef892b0b86002
Keren Mezuman <km2961@columbia.edu>
2024-10-13 21:33:49 -0400

    Added echo statements to inform the user and comments in the code

--- subddcompare.bash
+++ subddcompare.bash
@@ -28,8 +28,9 @@ simname="E6F40tE3km"
 # Dynamically determine the number of days in the specified month and year
 days_in_month=$(cal $month $year | awk 'NF {DAYS = $NF}; END {print DAYS}')
 
-# Output file to store combined data
+# Temporary output file to store combined data
 outputfile="out.nc"
+echo "Output file path: ${outputfile}"
 
 # Loop over each diagnostic type
 for diag in ${diags}; do
@@ -57,15 +58,18 @@ for diag in ${diags}; do
 
       # Use ncks to append the current day's data for the specific species to the output file
       # -h: no header output, -A: append, -v: extract variable
+      echo "Appending data from ${subdd} to ${outputfile}"
       ncks -h -A -v ${specie} ${subdd} ${outputfile}
 
     done
 
     # Use ncap2 to calculate the mean for the current species across all sub-daily files
     # -s: script to perform the operation, "avg()" calculates the mean, result saved to "subddmean.nc"
+    echo "Calculating mean for ${specie} and saving to subddmean.nc"
     ncap2 -h -O -s "mean=avg(${specie})" ${outputfile} "subddmean.nc"
 
     # Append the monthly data to the file containing the mean of sub-daily data
+    echo "Appending monthly data from ${monthly} to subddmean.nc"
     ncks -h -A -v ${monthly_specie} ${monthly} "subddmean.nc"
 
   done
