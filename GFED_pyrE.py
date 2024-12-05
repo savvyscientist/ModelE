0349a5ba213f50f744805623ca298feab08da3c3
Keren Mezuman <km2961@columbia.edu>
2024-12-04 21:49:03 -0500

    Added tiered experiements, functionalized GFED4s and modelE emissions

--- GFED_pyrE.py
+++ GFED_pyrE.py
@@ -144,6 +144,54 @@ def calculate_spawnemis(vrang, BA, file_path, f, lats, lons, zero_mat):
     return emis_type
 
 ################################################################
+def modelE_emis(year)
+    # Read pyrE CO2n emissions
+    ann_sum_pyrE = 0
+    
+    # Loop over each month and read the corresponding file
+    for month in months:
+        # Construct the filename for each month
+        tracer_filename = f"{month}{year}.taijnk_CCycle_E6obioF40.nc"
+        filepath = os.path.join(config['dir_sim'], tracer_filename)
+        if os.path.exists(filepath):
+            with nc.Dataset(filepath) as f:
+                emis_pyrE = f.variables['CO2n_pyrE_src'][:]
+                units = f.variables['CO2n_pyrE_src'].units
+                scaling_factor, unit = extract_scaling_factor(units)
+                emis_pyrE *= float(scaling_factor)#kgCO2 m-2 s-1
+                ndays = calendar.monthrange(year,months.index(month)+1)[1]
+                emis_pyrE *= (ndays * s_in_day)  #kgCO2 m-2 yr-1
+                emis_pyrE *= kgtog #gCO2 m-2 M-1
+                ann_sum_pyrE += emis_pyrE #gCO2 m-2 yr-1  
+            tot_emis_pyrE = np.nansum(ann_sum_pyrE * axyp) #gCO2 yr-1
+            tot_emis_pyrE = format(tot_emis_pyrE, '.3e')
+        else:
+            print(f"File {filepath} not found. Skipping.")
+    return ann_sum_pyrE,tot_emis_pyrE
+
+###############################################################
+def GFED4s_emis(year)
+    # Calculate GFED4s annual mean emissions for comparison
+    obs_filepath = os.path.join(config['dir_obs_emis'], f"{year}.nc")  # Example: Use actual filenames here
+
+    if os.path.exists(obs_filepath):
+        ann_sum = np.zeros((config['nlat'], config['nlon']), dtype=float)
+        with nc.Dataset(obs_filepath) as f_obs:
+            for k in range(12):
+                GFED_data = f_obs.variables['CO2n'][k, :, :]#[kg m-2 s-1]
+                GFED_CO2 = GFED_data.reshape(config['nlat'], config['nlon'])
+                GFED_CO2 = np.where(GFED_CO2 <= 0., zero_mat, GFED_CO2)
+                ndays = calendar.monthrange(year,k+1)[1]
+                ann_sum += (GFED_CO2 * ndays * s_in_day) #kgCO2 m-2 M-1
+            ann_sum *= kgtog #gCO2 m-2 yr-1
+            tot_GFED = np.nansum(ann_sum*axyp) #gCO2 yr-1
+            tot_GFED = format(tot_GFED, '3e')
+
+    else:
+        print(f"File {obs_filepath} not found. Skipping.")
+    return ann_sum, tot_GFED
+
+###############################################################
 
 #to do: fix function so that total is printed in the title
 def define_subplot(fig, ax, data, lons, lats, cmap, cborientation, fraction, pad, labelpad, fontsize, title, clabel, masx, is_diff=False,glob=None):
@@ -201,7 +249,8 @@ def process_data():
     # using pyrE BA and assumption of 100% combustion completeness
     for year in range(config['iyear'], config['fyear'] + 1):
         veg_filename = f"ANN{year}.aijnk_CCycle_E6obioF40.nc"
-        filepath = os.path.join(config['dir_sim'], veg_filename)
+        filepath = os.path.join(config['dir_sim'], 'ANN_aij', veg_filename)
+        print(filepath)
         zero_mat = np.zeros((config['nlat'], config['nlon']), dtype=float)
     
         if os.path.exists(filepath):
@@ -219,7 +268,8 @@ def process_data():
                 
                 # Calculate emissions using Ent biomass and pyrE BA
                 # and calculate emissions using Spawn biomass and pyrE BA
-                BA_grass = f.variables['BA_grass'] #m^2
+                BA_grass = f.variables['BA_grass'] #m^2 
+                #BA_grass = np.where(BA_grass < 0., zero_mat, BA_grass)
 
                 emis_G = calculate_emis(vrang=config['grang'], BA=BA_grass, f=f, missing_val=None, nan_mat=None, 
                                         lats=lats, lons=lons, zero_mat=zero_mat, experiment=exp)
@@ -228,6 +278,7 @@ def process_data():
                                                 f=f, lats=lats, lons=lons, zero_mat=zero_mat)
 
                 BA_shrub = f.variables['BA_shrub'] #m^2
+                #BA_shrub = np.where(BA_shrub < 0., zero_mat, BA_shrub)
 
                 emis_S = calculate_emis(vrang=config['srang'], BA=BA_shrub, f=f, missing_val=None, nan_mat=None, 
                                         lats=lats, lons=lons, zero_mat=zero_mat, experiment=exp)
@@ -236,6 +287,8 @@ def process_data():
                                                 f=f, lats=lats, lons=lons, zero_mat=zero_mat)
 
                 BA_tree = f.variables['BA_tree'] #m^2
+                #BA_tree = np.where(BA_tree < 0., zero_mat, BA_tree)
+
 
                 emis_T = calculate_emis(vrang=config['trang'], BA=BA_tree, f=f, missing_val=None, nan_mat=None, 
                                         lats=lats, lons=lons, zero_mat=zero_mat, experiment=exp)
@@ -331,9 +384,108 @@ def process_data():
     plt.tight_layout()
     plt.show()
 
+def tiered_experiments():
+    config = load_config()
+    zero_mat = np.zeros((config['nlat'], config['nlon']), dtype=float)
+    kgCtogCO2 = 44./12.*1000.
+    kgtog = 1000.
+    s_in_day = 60.*60.*24.
+    s_in_yr = s_in_day*365.
+    # List of months to read from the monthly files
+    months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+    cbar_max = 900.
+    #all aboveground
+    #litter fine (ra032 + ra033 + ra037): 
+    #litter fine + litter CWD (ra036)
+    #litter finer + litter CWD + foliage (ra019)
+    #GFED4s based: Table 1 + soil moisture index (SMI)
+    #GFED4s include fuel load from leaves, stems, fine leaf litter, CWD, Boreal organic soils, Tropical peat organic soils
+    #GFED4s combustion completeness varies between a min and max values (per fuel type) and is scaled to the "soil moisture index" etc...
+    exp = ['all','lfine&CWD&fol','lfine&CWD','lfine']
+
+    fig, ax = plt.subplots(2, 3, figsize=(18, 10), subplot_kw={'projection': ccrs.PlateCarree()})
+    # Calculate emissions from Ent Biomass
+    # using pyrE BA and assumption of 100% combustion completeness
+    for year in range(config['iyear'], config['fyear'] + 1):
+        veg_filename = f"ANN{year}.aijnk_CCycle_E6obioF40.nc"
+        filepath = os.path.join(config['dir_sim'], 'ANN_aij', veg_filename)
+        print(filepath)
+        zero_mat = np.zeros((config['nlat'], config['nlon']), dtype=float)
+    
+        if os.path.exists(filepath):
+            for i,e in enumerate(exp):
+                # Initialize arrays for emissions from different datasets
+                emis_G = np.zeros((config['nlat'], config['nlon']), dtype=float)
+                emis_S = np.zeros((config['nlat'], config['nlon']), dtype=float)
+                emis_T = np.zeros((config['nlat'], config['nlon']), dtype=float)
+                emis_sp_G = np.zeros((config['nlat'], config['nlon']), dtype=float)
+                emis_sp_S = np.zeros((config['nlat'], config['nlon']), dtype=float)
+                emis_sp_T = np.zeros((config['nlat'], config['nlon']), dtype=float)
+                with nc.Dataset(filepath) as f:
+                    lats = f.variables['lat'][:]
+                    lons = f.variables['lon'][:]
+                    axyp = f.variables['axyp'][:]
+                
+                    # Calculate emissions using Ent biomass and pyrE BA
+                    # and calculate emissions using Spawn biomass and pyrE BA
+                    BA_grass = f.variables['BA_grass'] #m^2 
+                    #BA_grass = np.where(BA_grass < 0., zero_mat, BA_grass)
+    
+                    emis_G = calculate_emis(vrang=config['grang'], BA=BA_grass, f=f, missing_val=None, nan_mat=None, 
+                                            lats=lats, lons=lons, zero_mat=zero_mat, experiment=e)
+
+                    BA_shrub = f.variables['BA_shrub'] #m^2
+                    #BA_shrub = np.where(BA_shrub < 0., zero_mat, BA_shrub)
+    
+                    emis_S = calculate_emis(vrang=config['srang'], BA=BA_shrub, f=f, missing_val=None, nan_mat=None, 
+                                            lats=lats, lons=lons, zero_mat=zero_mat, experiment=e)
+
+                    BA_tree = f.variables['BA_tree'] #m^2
+                    #BA_tree = np.where(BA_tree < 0., zero_mat, BA_tree)
+
+                    emis_T = calculate_emis(vrang=config['trang'], BA=BA_tree, f=f, missing_val=None, nan_mat=None, 
+                                            lats=lats, lons=lons, zero_mat=zero_mat, experiment=e)
+
+                    emis_tot = emis_G + emis_S + emis_T #kgC yr-1
+                    emis_tot *= kgCtogCO2 #gCO2 yr-1
+                    tot_emis = np.nansum(emis_tot) #gCO2 yr-1
+                    tot_emis = format(tot_emis, '.3e')
+                    emis_tot /= axyp #gCO2m-2 yr-1
+            # Plot emissions from pyrE BA and Ent Biomass 
+            #to do: fix the index of subfigure
+            define_subplot(fig, ax[i, 0], emis_tot, lons, lats, cmap=config['cmap'], 
+                   cborientation='horizontal', fraction=0.05, pad=0.05, 
+                   labelpad=5, fontsize=10, title=f'Offline Emissions (pyrE BA and Ent Biomass)\nTotal:{tot_emis}[gCO2]', 
+                   clabel='CO2 Emissions [gCO2 m-2 yr-1]', masx=cbar_max)#0.7*emis_tot.max())
+
+        else:
+            print(f"File {filepath} not found. Skipping.")
+
+        (ann_sum_pyrE,tot_emis_pyrE) = modelE_emis(year)
+        (ann_sum,tot_GFED) = GFED4s_emis(year)
+
+    # to do: once the GFED4s and GFED5 BA is regridded emissions can be calculated with both the Ent and Spawn biomass
+    
+
+    # Plot emissions from pyrE 
+    define_subplot(fig, ax[1, 0], ann_sum_pyrE, lons, lats, cmap=config['cmap'], 
+                   cborientation='horizontal', fraction=0.05, pad=0.05, 
+                   labelpad=5, fontsize=10, title=f'pyrE fire based emissions\nTotal:{tot_emis_pyrE}[gCO2]', 
+                   clabel='CO2 Emissions [gCO2 m-2 yr-1]', masx=cbar_max)#0.7*ann_sum_pyrE.max())
+
+    # Plot emissions from GFED4s
+    define_subplot(fig, ax[1, 1], ann_sum, lons, lats, cmap=config['cmap'], 
+                   cborientation='horizontal', fraction=0.05, pad=0.05, 
+                   labelpad=5, fontsize=10, title=f'GFED4s emissions\nTotal:{tot_GFED}[gCO2]', 
+                   clabel='CO2 Emissions [gCO2 m-2 yr-1]', masx=cbar_max)#0.7*ann_sum.max())
+
+    plt.tight_layout()
+    plt.show()
+
 ################################################################
 
 # Run the main process
 if __name__ == "__main__":
-    process_data()
+    #process_data()
+    tiered_experiments()
 
